3b721d1f2d6f29a4c1e18cc054fb1717f39d9305
kkilme <jojo5142@naver.com>
2025-06-01 07:39:10 +0900

    chore: Store UI 디자인 및 GridLayout관련 Util 컴포넌트
    UI_CenteredGridLayout.cs 참조

--- Assets/C#/UI/Utils/UI_Billboard.cs
+++ Assets/C#/UI/Utils/UI_Billboard.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 
 /// <summary>
-/// 카메라 방향을 계속 바라보는 UI에 부착
+/// 카메라 방향을 계속 바라보는 UI에 부착하는 컴포넌트. 주로 World Space UI에 사용.
 /// </summary>
 public class UI_Billboard : MonoBehaviour
 {
---
+++ Assets/C#/UI/Utils/UI_CenteredGridLayout.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// GridLayoutGroup이 한 행일 때도 childAlignment = UpperCenter의 정렬을 유지하며 자식들을 좌측 상단부터 배치하도록 강제하는 컴포넌트.
+/// </summary>
+// 가정: StartCorner = UpperLeft, Axis = Horizontal, Constraint = Flexible
+[ExecuteAlways]
+[RequireComponent(typeof(GridLayoutGroup))]
+public class UI_CenteredGridLayout : MonoBehaviour
+{
+    private void OnEnable() => UpdateLayout();
+    private void OnTransformChildrenChanged() => UpdateLayout();
+
+    private void OnRectTransformDimensionsChange() => UpdateLayout();
+
+    #if UNITY_EDITOR
+    private void OnValidate() => UpdateLayout();
+    #endif
+
+    private void UpdateLayout()
+    {
+        GridLayoutGroup grid = GetComponent<GridLayoutGroup>();
+        RectTransform rect = GetComponent<RectTransform>();
+
+        // 활성 자식만 카운트
+        int activeChildCount = transform.Cast<Transform>().Count(t => t.gameObject.activeSelf);
+
+        float totalWidth = rect.rect.width;
+        float cellWidth = grid.cellSize.x;
+        float spacing = grid.spacing.x;
+
+        // totalWidth + spacing: 마지막 셀 뒤는 spacing이 필요하지 않다는 점을 반영
+        int itemsPerRow = Mathf.FloorToInt((totalWidth + spacing) / (cellWidth + spacing));
+        itemsPerRow = Mathf.Max(itemsPerRow, 1);
+
+        if (activeChildCount >= itemsPerRow)
+        {
+            grid.childAlignment = TextAnchor.UpperCenter;
+            grid.padding.left = 0;
+        }
+        else
+        {
+            grid.childAlignment = TextAnchor.UpperLeft;
+
+            float rowPreferredWidth = itemsPerRow * cellWidth + (itemsPerRow - 1) * spacing;
+            grid.padding.left = Mathf.RoundToInt((totalWidth - rowPreferredWidth) / 2);
+        }
+    }
+}
